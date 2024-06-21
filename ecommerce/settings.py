3a8852dac116dd1636b169936fef4751784e7d1d
Francesco Gori <francesco.gori7@edu.unifi.it>
2024-06-21 18:59:26 +0200

    staticfiles 2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,9 +134,9 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_URL = 'staticfiles/images/'
+MEDIA_URL = 'static/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
